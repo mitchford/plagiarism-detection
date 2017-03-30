74b74c923d49462ac8f9fe4097c2b040ddeda639
mitch <mitch.ford1@gmail.com>
2017-03-30 19:52:23 +0100

    added levenschtein distance algorithm and tidied up code

--- src/plagiarism/Plagiarism.java
+++ src/plagiarism/Plagiarism.java
@@ -7,11 +7,25 @@ package plagiarism;
 
 import java.util.Scanner;
 
+
 /**
  *
  * @author mitchford
  */
 public class Plagiarism {
+    
+//    public void db(){
+//    
+//    try {
+//        Driver myDriver = new com.mysql.jdbc.Driver();
+//        DriverManager.registerDriver( myDriver );
+//    }
+//    catch(ClassNotFoundException ex) {
+//        System.out.println("Error: unable to load driver class!");
+//        System.exit(1);
+//    }
+//        
+//    } 
 
     /* Function taken from 
     https://github.com/mission-peace/interview/blob/master/src/com/interview/string/SubstringSearch.java */ 
@@ -75,6 +89,43 @@ public class Plagiarism {
         return words + 1;
     }
     
+    public int levd(char[] str1, char[] str2){
+
+        int d[][] = new int [str1.length+1][str2.length+1];
+
+        for(int i = 0; i < d[0].length; i++){
+          d[0][i] = i;
+        }
+
+        for(int i = 0; i < d.length; i++){
+          d[i][0] = i;
+        }
+
+        for(int i = 1; i <= str1.length; i++){
+            for(int j = 1; j <= str2.length; j++){
+                if(str1[i-1] == str2[j-1]){
+                    d[i][j] = d[i-1][j-1];
+                } else {
+                    d[i][j] = 1 + getMin(d[i-1][j-1], d[i-1][j], d[i][j-1]);
+                }
+            }
+        }
+
+        return d[str1.length][str2.length];
+    }
+
+    public int getMin(int a, int b, int c){
+      int lowest = a;
+      if(b<a){
+        lowest = b;
+      }
+      if(c<lowest){
+        lowest = c;
+      }
+      return lowest;
+    }
+
+    
     
     public static void main(String[] args) {
         
@@ -83,15 +134,23 @@ public class Plagiarism {
         System.out.print("enter string: ");
         String user_string = scan.nextLine();
         String test_string = "Test file. This will hopefully pick up matches. three sentences.";
-        int matchCount = 0;
+        String levtest = "test fils";
+        int matchCount = 0, similarities = 0;
         int result = search.wordCount(user_string);
-        float percent = 0;
+        float percent, percent2;
+        int dist;
+        int allowance = 10;
+        
     
         //Separate the text file into sentences
         String[] submission = user_string.split("\\.");
         for (int i = 0; i < submission.length; i++) {
+            dist = search.levd(submission[i].trim().toLowerCase().toCharArray(), levtest.trim().toLowerCase().toCharArray());
+            if (dist < allowance) {
+                similarities++;
+            }
             // need to replace user_string variable with text from db
-            boolean matched = search.KMP(test_string.toCharArray(), submission[i].toCharArray());
+            boolean matched = search.KMP(test_string.trim().toLowerCase().toCharArray(), submission[i].trim().toLowerCase().toCharArray());
             if (matched) {
                 // keep a count of matched strings
                 matchCount++;
@@ -99,7 +158,8 @@ public class Plagiarism {
         }
         // work out % of matched material for judgement
         percent = ((float) matchCount/(float) submission.length) * 100;
-        System.out.println(percent + "% matched. word count: " + result);
+        percent2 = ((float) similarities/(float) submission.length) * 100;
+        System.out.println(percent + "% matched exactly." + percent2 + "% similar (based on allowance). Word count: " + result);
         
     }
     
